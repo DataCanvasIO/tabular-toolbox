c973f33e1bc2aeacb048c62acb662c89318890c2
lixfz <lixf@sovon.net>
2020-11-03 10:34:37 +0800

    Add method to load 'bank' dataset as dask dataframe

--- tabular_toolbox/datasets/dsutils.py
+++ tabular_toolbox/datasets/dsutils.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
-import pandas as pd
 import os
 
 basedir = os.path.dirname(__file__)
 
 
 def load_bank():
+    import pandas as pd
     data = pd.read_csv(f'{basedir}/bank-uci.csv.gz')
+    return data
+
+
+def load_bank_by_dask():
+    from dask import dataframe as dd
+    data = dd.read_csv(f'{basedir}/bank-uci.csv.gz', compression='gzip')
     return data
