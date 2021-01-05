3ec10dc7808402cc0b94c992055c25022be618c8
lixfz <lixf@sovon.net>
2021-01-05 16:30:09 +0800

    Fix hash_dataframe

--- tabular_toolbox/utils/_common.py
+++ tabular_toolbox/utils/_common.py
@@ -1,14 +1,14 @@
 import hashlib
 
-import dask.array as da
 import dask.dataframe as dd
 import numpy as np
 import pandas as pd
 
-from ..const import *
 from . import logging
+from ..const import *
 
 logger = logging.get_logger(__name__)
+
 
 def infer_task_type(y):
     if len(y.shape) > 1 and y.shape[-1] > 1:
@@ -54,14 +54,10 @@ def hash_dataframe(df, method='md5', index=False):
 
     if isinstance(df, dd.DataFrame):
         x = df.map_partitions(lambda part: pd.util.hash_pandas_object(part, index=index),
-                              meta=(None, 'u8'))
-        percentiles = da.percentile(x.values, range(0, 100)).compute()
+                              meta=(None, 'u8')).compute()
     else:
         x = pd.util.hash_pandas_object(df, index=index)
-        percentiles = np.percentile(x.values, range(0, 100))
 
-    for n in percentiles:
-        # m.update(struct.pack('d', n))
-        m.update(n)
+    np.vectorize(m.update, otypes=[None], signature='()->()')(x.values)
 
     return m.hexdigest()
