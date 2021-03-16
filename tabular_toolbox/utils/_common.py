23bb404f8d5a06ae7db93526ba36d7e47b3b2839
lixfz <lixf@sovon.net>
2021-03-16 20:47:15 +0800

    Required dask-ml as extra

--- tabular_toolbox/utils/_common.py
+++ tabular_toolbox/utils/_common.py
@@ -57,7 +57,7 @@ def hash_dataframe(df, method='md5', index=False):
     m = getattr(hashlib, method)()
 
     for col in df.columns:
-        m.update(col.encode())
+        m.update(str(col).encode())
 
     if isinstance(df, dd.DataFrame):
         x = df.map_partitions(lambda part: pd.util.hash_pandas_object(part, index=index),
